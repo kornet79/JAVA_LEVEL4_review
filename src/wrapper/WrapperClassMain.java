110ebb4252f50648ed2bf7cb790bcbc3988e5228
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-06-25 17:37:18 +0900

    Add files via upload

---
+++ src/wrapper/WrapperClassMain.java
@@ -0,0 +1,12 @@
+package immutable.wrapper;
+
+public class WrapperClassMain {
+
+    public static void main(String[] args) {
+//        Integer newInteger = new Integer(10);
+        Integer newInteger = 10;
+        Integer integerObj = Integer.valueOf(10);
+
+        Long newlong = 100L;
+    }
+}
---
+++ src/wrapper/오늘의_문제/문제2.java
@@ -0,0 +1,5 @@
+package immutable.wrapper.오늘의_문제;
+
+public class 문제2 {
+
+}
