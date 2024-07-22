259546047e446e9f1381e1ec0bee232899fc24f4
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-07-22 14:38:49 +0900

    Add files via upload

--- src/enumeration/ex1/AuthGradeMain2.java
+++ src/enumeration/ex1/AuthGradeMain2.java
@@ -1,8 +1,27 @@
 package immutable.enumeration.test.ex1;
 
+import java.util.Scanner;
+
 public class AuthGradeMain2 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("당신의 등급을 입력하세요[GUEST, LOGIN, ADMIN] : ");
+        String grade = sc.nextLine();
+        
+        AuthGrade authGrade = AuthGrade.valueOf(grade.toUpperCase());
+        System.out.println("당신의 등급은  " + authGrade.getdesc() + " 입니다. ");
+
+        System.out.println("== 메뉴 등급 ==");
+        if(authGrade.getLevel() > 0) {
+            System.out.println(" - 메인 화면");
+        }
+        if(authGrade.getLevel() > 1) {
+            System.out.println(" - 이메일 관리 화면");
+        }
+        if(authGrade.getLevel() > 2) {
+            System.out.println(" - 관리자 화면");
+        }
     }
 }
