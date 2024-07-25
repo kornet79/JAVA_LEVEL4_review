bdb6eba671f9c32ff9aace30d13161b768251991
GyuMin <126860086+kornet79@users.noreply.github.com>
2024-07-25 11:30:13 +0900

    Delete src/level4_형준쌤 directory

--- src/level4_형준쌤/Employee.java
+++
@@ -1,6 +0,0 @@
-package level4_형준쌤;
-
-public interface Employee {
-    double calcullateSalary();
-    EmployeeType getEmployeeType();
-}
--- src/level4_형준쌤/TestEmployee.java
+++
@@ -1,7 +0,0 @@
-package level4_형준쌤;
-
-public class TestEmployee {
-    public static void main(String[] args) {
-
-    }
-}
